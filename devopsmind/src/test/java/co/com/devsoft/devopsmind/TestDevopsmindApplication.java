19c652831c7a7e289ecba3ab5cf735ed7511686d
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2026-09-16 23:27:50 -0500

    refactor:Ajusta nombres para mejor legibilidad

--- devopsmind/src/test/java/co/com/devsoft/devopsmind/TestDevopsmindApplication.java
+++ devopsmind/src/test/java/co/com/devsoft/devopsmind/TestDevopsmindApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestDevopsmindApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(DevopsmindApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
