98dafdda67cf5f3c63579872bfe2daa4db3fd972
Wilman Ortiz Navarro <willman.ortiz@gmail.com>
2026-09-16 22:10:47 -0500

    fix: ajusta variables de entorno para el funcionamiento de los placeholders de devopsmind

--- devopsmind/scripts/credentials.sh
+++ devopsmind/scripts/credentials.sh
@@ -13,12 +13,25 @@ fi
 echo "🔐 Extract credentials directly from VAULT to memory..."
 
 # Definir las rutas en VAULT
-VAULT_DB_USER_PATH="development/database/postgresql/username"
-VAULT_DB_PASS_PATH="development/database/postgresql/password"
-VAULT_DB_HOST_PATH="development/database/postgresql/hostname"
-VAULT_DB_NAME_PATH="development/database/postgresql/database"
-VAULT_DB_SCHEMA_PATH="development/database/postgresql/schema"
-VAULT_API_AUTH_SECRET_KEY_PATH="development/api/auth/secret-key"
+VAULT_API_OLLAMA_SECRET_KEY=$(gopass show -o ai/api/auth/secret-key)
+VAULT_DB_MYSQL_ROOT_PASSWORD=$(gopass show -o ai/database/mysql/password)
+VAULT_DB_MYSQL_USERNAME=$(gopass show -o ai/database/mysql/username)
+VAULT_DB_MYSQL_PASSWORD=$(gopass show -o ai/database/mysql/password)
+VAULT_DB_MYSQL_HOSTNAME=$(gopass show -o ai/database/mysql/hostname)
+VAULT_DB_MYSQL_DATABASE_NAME=$(gopass show -o ai/database/mysql/database)
+VAULT_DB_MYSQL_SCHEMA_NAME=$(gopass show -o ai/database/mysql/schema)
+VAULT_DB_POSTGRES_USERNAME=$(gopass show -o ai/database/postgresql/username)
+VAULT_DB_POSTGRES_PASSWORD=$(gopass show -o ai/database/postgresql/password)
+VAULT_DB_POSTGRES_HOSTNAME=$(gopass show -o ai/database/postgresql/hostname)
+VAULT_DB_POSTGRES_VECTOR_NAME=$(gopass show -o ai/database/postgresql/database)
+VAULT_DB_PGADMIN_EMAIL=$(gopass show -o ai/database/pgadmin/email)
+VAULT_DB_PGADMIN_PASSWORD=$(gopass show -o ai/database/pgadmin/password)
+VAULT_DB_MONGO_ROOT_USERNAME=$(gopass show -o ai/database/mongo/username)
+VAULT_DB_MONGO_ROOT_PASSWORD=$(gopass show -o ai/database/mongo/password)
+VAULT_DB_MONGO_HOSTNAME=$(gopass show -o ai/database/mongo/hostname)
+VAULT_DB_MONGO_INITIAL_DB=$(gopass show -o ai/database/mongo/database)
+VAULT_DB_MONGOEX_ADMIN_USERNAME=$(gopass show -o ai/database/mongoex/username)
+VAULT_DB_MONGOEX_ADMIN_PASSWROD=$(gopass show -o ai/database/mongoex/password)
 
 if ! command -v gopass &> /dev/null; then
     echo "❌ Error: gopass is not installed in the host machine." >&2
@@ -26,11 +39,24 @@ if ! command -v gopass &> /dev/null; then
 fi
 
 # Exportar directamente a las variables de entorno locales
-export DB_USERNAME=$(gopass show -o "$VAULT_DB_USER_PATH")
-export DB_PASSWORD=$(gopass show -o "$VAULT_DB_PASS_PATH")
-export DB_HOSTNAME=$(gopass show -o "$VAULT_DB_HOST_PATH")
-export DB_NAME=$(gopass show -o "$VAULT_DB_NAME_PATH")
-export DB_SCHEMA=$(gopass show -o "$VAULT_DB_SCHEMA_PATH")
-export API_SECRET_KEY=$(gopass show -o "$VAULT_API_AUTH_SECRET_KEY_PATH")
+export API_OLLAMA_SECRET_KEY=$(gopass show -o $VAULT_API_OLLAMA_SECRET_KEY)
+export DB_MYSQL_ROOT_PASSWORD=$(gopass show -o $VAULT_DB_MYSQL_ROOT_PASSWORD)
+export DB_MYSQL_USERNAME=$(gopass show -o $VAULT_DB_MYSQL_USERNAME)
+export DB_MYSQL_PASSWORD=$(gopass show -o $VAULT_DB_MYSQL_PASSWORD)
+export DB_MYSQL_HOSTNAME=$(gopass show -o $VAULT_DB_MYSQL_HOSTNAME)
+export DB_MYSQL_DATABASE_NAME=$(gopass show -o $VAULT_DB_MYSQL_DATABASE_NAME)
+export DB_MYSQL_SCHEMA_NAME=$(gopass show -o $VAULT_DB_MYSQL_SCHEMA_NAME)
+export DB_POSTGRES_USERNAME=$(gopass show -o $VAULT_DB_POSTGRES_USERNAME)
+export DB_POSTGRES_PASSWORD=$(gopass show -o $VAULT_DB_POSTGRES_PASSWORD)
+export DB_POSTGRES_HOSTNAME=$(gopass show -o $VAULT_DB_POSTGRES_HOSTNAME)
+export DB_POSTGRES_VECTOR_NAME=$(gopass show -o $VAULT_DB_POSTGRES_VECTOR_NAME)
+export DB_PGADMIN_EMAIL=$(gopass show -o $VAULT_DB_PGADMIN_EMAIL)
+export DB_PGADMIN_PASSWORD=$(gopass show -o $VAULT_DB_PGADMIN_PASSWORD)
+export DB_MONGO_ROOT_USERNAME=$(gopass show -o $VAULT_DB_MONGO_ROOT_USERNAME)
+export DB_MONGO_ROOT_PASSWORD=$(gopass show -o $VAULT_DB_MONGO_ROOT_PASSWORD)
+export DB_MONGO_HOSTNAME=$(gopass show -o $VAULT_DB_MONGO_HOSTNAME)
+export DB_MONGO_INITIAL_DB=$(gopass show -o $VAULT_DB_MONGO_INITIAL_DB)
+export DB_MONGOEX_ADMIN_USERNAME=$(gopass show -o $VAULT_DB_MONGOEX_ADMIN_USERNAME)
+export DB_MONGOEX_ADMIN_PASSWROD=$(gopass show -o $VAULT_DB_MONGOEX_ADMIN_PASSWROD)
 
 echo "✅ Variables [DB_USERNAME, DB_PASSWORD, DB_HOSTNAME, DB_NAME, DB_SCHEMA, API_SECRET_KEY] have been loaded succesfully in terminal environment..."
